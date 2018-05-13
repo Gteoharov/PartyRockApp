855e76a11a752ca6d9f74100675c15e7d430ce4a
Gteoharov <Gteoharov@users.noreply.github.com>
2018-05-13 17:29:55 +0300

    Creating the app

---
+++ PartyRockApp/Controller/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Georgi Teoharov on 13.05.18.
+//  Copyright © 2018 Georgi Teoharov. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
